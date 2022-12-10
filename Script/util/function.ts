f0be86dcccdd01b8f070aa57ca9d3eb597905a2f
chunqian.shen <chunqian.shen@qq.com>
2022-12-10 15:26:43 +0800

    补上Function混合接口扩展

--- Script/util/function.ts
+++ Script/util/function.ts
@@ -2,15 +2,18 @@
 
 // ******************************** 函数静态方法 ********************************
 
-interface IFunction extends Function {
+interface FunctionConstructor_ext {
   empty: () => void
 }
+
+interface IFunctionConstructor extends FunctionConstructor, FunctionConstructor_ext {}
+
+interface IFunction extends Function {}
 
 type IFunction_empty_func = (() => void)
 
 // 函数静态方法 - 空函数
-// Function 是混合类型接口, 先断言为Function类型, 再断言为IFunction类型
-const IFunction = (Function as Function) as IFunction
+const IFunction = <IFunctionConstructor>Function
 
 IFunction.empty = () => {}
 
